28f3b4d0f142fc4a557e258d307ee9439db7353b
2Sravs <sravani.chekuri1243#gmail.com>
2023-08-03 08:43:46 +0530

    firstmaster

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppParameterizedConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student stud1 = new Student(12, "Sam", new DateTime(day: 12, month: 09, year: 2010));
+            //Student stud2 = new Student(11, "Deep", new DateTime(day: 09, month: 11,year: 2009));
+            //Console.WriteLine("*** Student one***");
+            //stud1.Display();
+            //Console.WriteLine("***Student two ***");
+            //stud2.Display();
+
+
+
+            Student student = new Student();
+            student.Display();
+            Student stud1 = new Student(12, "Sam", new DateTime(day: 12, month: 09, year: 2010));
+            stud1.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppParameterizedConstructor
+{ 
+    
+   public class Student
+    {
+
+        int id;
+        string name;
+        DateTime dob;
+            
+        public Student()
+            
+        {
+               Console.WriteLine("Default Constructor");
+            id = -1;
+            name = "not given";
+            dob = DateTime.Now;
+
+        }              
+
+           
+        public Student(int id, string name, DateTime dob)
+            
+        {
+                Console.WriteLine("Parameterized Constructor of Student");
+                this.id = id;
+                this.name = name;
+                this.dob = dob;
+           
+        }
+           
+        public void Display()
+            {
+                Console.WriteLine("Student Details as Follows!!!");
+                Console.WriteLine("ID:->" + id+"\t Name:->" + name + "\t Date of Birth:->" + dob);
+            }
+        }
+    }
